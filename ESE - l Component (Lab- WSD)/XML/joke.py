41f07323f6af82aba0ab88ae28890fb05b75e4b2
arunothsymen <141496908+arunothsymen@users.noreply.github.com>
2023-09-21 22:44:20 +0530

    joke.py

--- ESE - l Component (Lab- WSD)/XML/joke.py
+++ ESE - l Component (Lab- WSD)/XML/joke.py
@@ -1,6 +1,6 @@
 from lxml import etree
-xml_tree = etree.parse("xml/jokes.xml")
-xsl_transform = etree.XSLT(etree.parse("xml/jokes.xsl"))
+xml_tree = etree.parse("xml/joke.xml")
+xsl_transform = etree.XSLT(etree.parse("xml/joke.xsl"))
 html_tree = xsl_transform(xml_tree)
 with open("symen.html", "wb") as output_file:
     output_file.write(etree.tostring(html_tree, pretty_print=True))
